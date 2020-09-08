e5b4a5e550d4aa6340cc2ee657d47a8adc1a9457
Maulik <maulikdholariya14@gmail.com>
2020-09-08 16:38:17 +0530

    .

---
+++ application/controllers/home.php
@@ -0,0 +1,35 @@
+<?php 
+
+class Home extends CI_Controller {
+
+
+    public function index(){
+
+
+        echo "This is home controller";
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
